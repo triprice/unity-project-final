85d333d491a8eb1080597090d8ee525c6989f3e7
itaykno <itayknow@gmail.com>
2023-07-01 21:21:30 +0300

    .

---
+++ projectContent/Assets/Scripts/PlayerScripts/PlayerInteract.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{ 
+
+    public Camera cam;
+    [SerializeField]
+    private float distance = 3f;
+    [SerializeField]
+    private LayerMask mask;
+    private PlayerUI playerUI;
+    public InputManager inputManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerUI = GetComponent<PlayerUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        playerUI.UpdateText(string.Empty);
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);   
+        Debug.DrawRay(ray.origin, ray.direction * distance);
+        RaycastHit hitInfo;
+       if(Physics.Raycast(ray, out hitInfo, distance, mask)){
+           if(hitInfo.collider.GetComponent<Interactable>() != null){
+           Interactable interactable = hitInfo.collider.GetComponent<Interactable>(); 
+           playerUI.UpdateText(interactable.promptMessage);
+                //if(inputManager.OnFoot.Interact.triggered){
+                 //    interactable.BaseInteract();  
+               // }
+           }
+       } 
+    }
+}   
